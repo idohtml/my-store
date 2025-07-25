de49b7ddc40a190e08d2a187e97ec0e97db17f7c
Jacob <Jacques.kanu@gmail.com>
2025-07-25 03:22:48 +0200

    user schema

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db/database"; // your drizzle instance
 import { nextCookies } from "better-auth/next-js";
+import * as schema from "@/db/schema";
+import { db } from "@/db/database"; // your drizzle instance
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
+    schema: {
+      user: schema.user,
+    },
   }),
   emailAndPassword: {
     enabled: true,
